35ec798fbb673ce4bb65ae8fe7fbba97590a5f50
Gabriela <gabifmouraroc@outlook.com>
2025-01-06 15:50:05 -0300

    actions create new user, login user

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -7,37 +7,65 @@ const SECRET_KEY = process.env.JWT_SECRET_KEY as string
 
 export async function createNewUser(data: typeSignUpSchema) {
     try {
-      const user = await db.user.findUnique({
-        where: {
-          email: data.email,
-        },
-      })
-  
-      if (user === null) {
-        const hashedPassword = await bcrypt.hash(data.password, 10)
-  
-        const user = await db.user.create({
-          data: {
-            name: data.name,
-            email: data.email,
-            password: hashedPassword,
-          },
+        const existingUser = await db.user.findUnique({
+            where: {
+                email: data.email,
+            },
         })
-  
-        const token = jwt.sign(
-          { id: user.id, email: user.email },
-          SECRET_KEY,
-          {
-            expiresIn: '1h',
-          },
-        )
-  
-        return { status: 'success', token }
-      } else {
-        return { status: 'notNull' }
-      }
+
+        if (existingUser) {
+            return { status: 'notNull' }
+        }
+
+        const hashedPassword = await bcrypt.hash(data.password, 10)
+
+        const user = await db.user.create({
+            data: {
+                name: data.name,
+                email: data.email,
+                password: hashedPassword,
+            },
+        })
+
+        return { status: 'success', 
+            user: { email: user.email, password: data.password }, }
+
     } catch (error) {
-      console.log(error)
-      return { status: 'error' }
+        console.log(error)
+        return { status: 'error' }
     }
-  }
+}
+
+export async function loginUser(email: string, password: string) {
+    try {
+        const user = await db.user.findUnique({
+            where: {
+                email: email,
+            },
+        })
+
+        if (!user) {
+            return { status: 'notFound' }; 
+        }
+
+        const isPasswordValid = await bcrypt.compare(user.password, password)
+
+        if (!isPasswordValid) {
+            return { status: 'invalidPassword' }; 
+        }
+
+        const token = jwt.sign(
+            { id: user.id, email: user.email },
+            SECRET_KEY,
+            {
+                expiresIn: '1h',
+            },
+        )
+
+        return { status: 'success', token }
+
+    } catch (error) {
+        console.log(error)
+        return { status: 'error' }
+    }
+}
