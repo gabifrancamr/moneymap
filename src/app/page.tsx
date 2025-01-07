2987710fd337e053203bac487fcbe400adcee8fb
Gabriela <gabifmouraroc@outlook.com>
2025-01-07 17:33:06 -0300

    components page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,40 @@
-import { Button, HStack } from "@chakra-ui/react";
+import { BtnSignIn } from "@/components/btnSignIn/BtnSignIn";
+import { BtnSignUp } from "@/components/btnSignUp/BtnSignUp";
+import HomepageBackground from "@/components/homepageBackground/HomepageBackground";
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <HStack>
-      <Button>Click me</Button>
-    </HStack>
+    <Box position="relative" width="100vw" height="100vh" overflow="hidden">
+      <HomepageBackground />
+
+      <Flex
+        position="absolute"
+        top="50%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+        zIndex="overlay"
+        textAlign="center"
+        color="white"
+        flexDirection={"column"}
+        gap={"8"}
+        alignItems={"center"}
+      >
+        <Box>
+          <Flex fontSize="5xl" fontWeight="bold" gap={"1"} justifyContent={"center"}>
+            <Text>Money</Text>
+            <Text color={"green.500"}>Map</Text>
+          </Flex>
+          <Text fontSize="lg" >
+            Manage your finances simply and smartly with Money Map!
+          </Text>
+        </Box>
+
+        <Flex gap={"2"}>
+          <BtnSignIn />
+          <BtnSignUp />
+        </Flex>
+      </Flex>
+    </Box>
   );
 }
