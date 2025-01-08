17b0b815bf605c72c7434f7ad42ac87a928f7801
Gabriela <gabifmouraroc@outlook.com>
2025-01-08 13:05:26 -0300

    type app

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+export enum TransactionType {
+    Income = "income",
+    Outcome = "outcome",
+}
+
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export interface FinancialInfo {
+    id: string;
+    userId: string;
+    name: string;
+    type: TransactionType;
+    value: number;
+    date: Date;
+    createdAt: Date;
+    updatedAt: Date;
+}
