d17de1ad501b5bea54621f6aedad2256f353eaff
Gabriela <gabifmouraroc@outlook.com>
2025-01-27 17:51:12 -0300

    styling summary

--- src/components/summary/Summary.tsx
+++ src/components/summary/Summary.tsx
@@ -16,12 +16,15 @@ export function Summary({ user }: SummaryProps) {
     return (
         <Box className="box-shadow" rounded={"md"} paddingY={"4"} paddingX={"6"} spaceY={"12"}>
             <Flex justifyContent={"space-between"} alignItems={"center"} gap={"8"}>
-                <Text fontSize={"3xl"}>Welcome, { user.name }</Text>
+                <Flex gap={"0.5rem"}>
+                    <Text color={"green.500"} fontSize={"3xl"}>Welcome,</Text>
+                    <Text fontSize={"3xl"}>{user.name}</Text>
+                </Flex>
                 <BtnNewTransaction />
             </Flex>
 
             <Flex justifyContent={"space-between"}>
-                <Box rounded="md" borderColor="green" borderWidth="1px" padding={"4"} paddingX={{base: '2', sm: '4', md: '12', lg: '20', xl: '24'}}>
+                <Box rounded="md" borderColor="green" borderWidth="1px" padding={"4"} paddingX={{ base: '2', sm: '4', md: '12', lg: '20', xl: '24' }}>
                     <StatRoot gap={"2"}>
                         <HStack justify="space-between">
                             <StatLabel fontSize={"lg"}>Total</StatLabel>
@@ -32,7 +35,7 @@ export function Summary({ user }: SummaryProps) {
                         <StatValueText fontSize={"3xl"}>{priceFromatter.format(summary.total)}</StatValueText>
                     </StatRoot>
                 </Box>
-                <Box rounded="md" borderColor="green" borderWidth="1px" padding={"4"} paddingX={{base: '2', sm: '4', md: '12', lg: '20', xl: '24'}}>
+                <Box rounded="md" borderColor="green" borderWidth="1px" padding={"4"} paddingX={{ base: '2', sm: '4', md: '12', lg: '20', xl: '24' }}>
                     <StatRoot gap={"2"}>
                         <HStack justify="space-between">
                             <StatLabel fontSize={"lg"}>Income</StatLabel>
@@ -40,15 +43,15 @@ export function Summary({ user }: SummaryProps) {
                                 <FaArrowTrendUp />
                             </Icon>
                         </HStack>
-                        <StatValueText fontSize={"3xl"}>{priceFromatter.format(summary.income)}</StatValueText>
+                        <StatValueText color={"green.500"} fontSize={"3xl"}>{priceFromatter.format(summary.income)}</StatValueText>
                     </StatRoot>
                 </Box>
-                <Box rounded="md" borderColor="green" borderWidth="1px" padding={"4"} paddingX={{base: '2', sm: '4', md: '12', lg: '20', xl: '24'}}>
+                <Box rounded="md" borderColor="green" borderWidth="1px" padding={"4"} paddingX={{ base: '2', sm: '4', md: '12', lg: '20', xl: '24' }}>
                     <StatRoot gap={"2"}>
                         <HStack justify="space-between">
                             <StatLabel fontSize={"lg"}>Outcome</StatLabel>
                             <Icon>
-                                <FaArrowTrendDown  />
+                                <FaArrowTrendDown />
                             </Icon>
                         </HStack>
                         <StatValueText color={"red.600"} fontSize={"3xl"}>{priceFromatter.format(summary.outcome)}</StatValueText>
