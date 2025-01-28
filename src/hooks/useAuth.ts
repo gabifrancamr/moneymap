9665d8826938604cb94ae56902df464e5851b595
Gabriela <gabifmouraroc@outlook.com>
2025-01-28 13:52:26 -0300

    refetch user

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,15 +1,22 @@
 "use client"
 
+import { useAppContext } from '@/contexts/AppContext';
 import cookie from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
 import { useRouter } from 'next/navigation';
 import { useCallback } from 'react';
 
 export function useAuth() {
     const router = useRouter();
+    const { refetchUser } = useAppContext()
 
-    const handleAuthentication = useCallback((token: string) => {
+    const handleAuthentication = useCallback(async (token: string) => {
         localStorage.setItem('token', token);
         cookie.set('token', token, { expires: 1 / 24, path: '/' });
+
+        const decodedToken = jwtDecode<{ email: string }>(token);
+        await refetchUser(decodedToken.email)
+
         router.push('/dashboard');
     }, [router]);
 
